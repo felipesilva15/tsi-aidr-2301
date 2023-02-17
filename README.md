8910f591d50002a72b64d36d608acfd22e5ec600
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2023-02-17 19:45:41 -0300

    Subindo arquivos iniciais do Angular para a aula 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Aula01
+# Aula02
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.5.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'aula01';
-
-  frase: string = 'Meu primeiro app Angular!';
+  title = 'aula02';
 }
