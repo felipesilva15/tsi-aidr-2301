9a2ce77644deb205fa6cd3f9d17df1fb1b301e16
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2023-02-17 21:57:07 -0300

    Subindo exercícios: Componentes e interpolação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateComponent } from './views/date/date.component';
+import { AboutComponent } from './views/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/date/date.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date',
+  templateUrl: './date.component.html',
+  styleUrls: ['./date.component.css']
+})
+export class DateComponent {
+  data: Date = new Date();
+}
