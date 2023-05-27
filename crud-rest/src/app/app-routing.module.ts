ff951f38e4d31e9283e5f60508689efc538e1d73
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2023-05-26 22:10:17 -0300

    Utilizando o Angular material para estilizar o projeto

--- crud-rest/src/app/app-routing.module.ts
+++ crud-rest/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ClienteComponent } from './views/cliente/cliente.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreComponent } from './views/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cliente', component: ClienteComponent },
+  { path: 'sobre', component: SobreComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- crud-rest/src/app/app.module.ts
+++ crud-rest/src/app/app.module.ts
@@ -6,17 +6,41 @@ import { AppComponent } from './app.component';
 import { ClienteComponent } from './views/cliente/cliente.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { SobreComponent } from './views/sobre/sobre.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular material modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteComponent
+    ClienteComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
